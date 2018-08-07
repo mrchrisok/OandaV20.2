b92823a6e613854a64920717f74ed6b754f69ea9
Osita Okonkwo <mrchrisok@hotmail.com>
2018-08-07 01:37:48 -0500

    added sample usage app

---
+++ OkonkwoOandaV20/OkonkwoOandaV20App/Program.cs
@@ -0,0 +1,181 @@
+﻿using OkonkwoOandaV20.Framework;
+using OkonkwoOandaV20.Framework.Factories;
+using OkonkwoOandaV20.TradeLibrary.REST;
+using OkonkwoOandaV20.TradeLibrary.REST.OrderRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static OkonkwoOandaV20.TradeLibrary.REST.Rest20;
+
+namespace OkonkwoOandaV20App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteNewLine("Hello trader! Welcome to OkonkwoOandaV20.2");
+
+            SetApiCredentials();
+
+            StartTransactionsStream();
+
+            PutOnATrade().Wait();
+
+            StopTransactionsStream();
+
+            Console.ReadKey();
+        }
+
+        static string AccountID { get; set; }
+        const string INSTRUMENT = InstrumentName.Currency.EURUSD;
+
+        static void SetApiCredentials()
+        {
+            WriteNewLine("Setting your V20 credentials ...");
+
+            AccountID = "101-001-1913854-002";
+            var environment = EEnvironment.Practice;
+            var token = "c9e9494d79013cac1d34f0e4dcb590cd-977a37b80762fb48cdb3b0b2e832628a";
+
+            Credentials.SetCredentials(environment, token, AccountID);
+
+            WriteNewLine("Nice! Credentials are set.");
+        }
+
+        #region trading
+        static private async Task PutOnATrade()
+        {
+            WriteNewLine("Checking to see if EUR_USD is open for trading ...");
+
+            // first, check the market status for EUR_USD
+            // if it is tradeable, we'll try to make some money :)
+            if (!(await Utilities.IsMarketHalted(INSTRUMENT)))
+            {
+                WriteNewLine("EUR_USD is open and rockin', so let's start trading!");
+
+                long? tradeID = await PlaceMarketOrder();
+
+                if (tradeID.HasValue)
+                {
+                    // we have an open trade.
+                    // give it some time to make money :)
+                    await Task.Delay(10000);
+
+                    WriteNewLine("Okay, we've waited 10 seconds. Closing trade now ...");
+
+                    // now, let' close the trade and collect our profits! .. hopefully
+                    TradeCloseResponse closeResponse = null;
+                    try
+                    {
+                        var parameters = new TradeCloseParameters() { units = "ALL" };
+                        closeResponse = await Rest20.PutTradeCloseAsync(AccountID, tradeID.Value, parameters);
+                    }
+                    catch
+                    {
+                        WriteNewLine("Oops. The trade can't be closed. Something went wrong. :(");
+                    }
+
+                    if (closeResponse != null)
+                    {
+                        WriteNewLine("Nice! The trade is closed.");
+
+                        var profit = closeResponse.orderFillTransaction.pl;
+                        WriteNewLine($"Our profit was USD {profit}");
+
+                        if (profit > 0)
+                            WriteNewLine($"Nice work! You are an awesome trader.");
+                        else
+                        {
+                            WriteNewLine($"Looks like you need to learn some money-making strategies. :(");
+                            WriteNewLine($"Keep studying, learning, but most of all .. keep trading!!");
+                        }
+                    }
+                }
+                else
+                {
+                    WriteNewLine($"Looks like something went awry with the trade. you need to learn some money-making strategies. :(");
+                }
+            }
+            else
+            {
+                WriteNewLine("Sorry, Oanda markets are closed or Euro market is not tradeable.");
+                WriteNewLine("Try again another time.");
+            }
+        }
+
+        static async Task<long?> PlaceMarketOrder(string side = "buy")
+        {
+            WriteNewLine("Creating a EUR_USD market BUY order ...");
+
+            var parameters = new AccountInstrumentsParameters() { instruments = new List<string>(){ INSTRUMENT } };
+            var oandaInstrument = (await Rest20.GetAccountInstrumentsAsync(AccountID, parameters)).First();
+            long orderUnits = side == "buy" ? 10 : -10;
+        
+            var request = new MarketOrderRequest(oandaInstrument)
+            {
+                units = orderUnits
+            };
+
+            PostOrderResponse response = null;
+            try
+            {
+                response = await Rest20.PostOrderAsync(AccountID, request);
+                WriteNewLine("Congrats! You've put on a trade! Let it run! :)");
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = ErrorResponseFactory.Create(ex.Message);
+
+                WriteNewLine("Oops. Order creation failed.");
+                WriteNewLine($"The failure message is: {errorResponse.errorMessage}.");
+                WriteNewLine("Try again later.");
+            }
+
+            return response?.orderFillTransaction?.tradeOpened?.tradeID;
+        }
+        #endregion
+
+        #region transactions stream
+        static Semaphore _transactionReceived;
+        static TransactionsSession _transactionsSession;
+
+        static void StartTransactionsStream()
+        {
+            WriteNewLine("Starting transactions stream ...");
+
+            _transactionsSession = new TransactionsSession(AccountID);
+            _transactionReceived = new Semaphore(0, 100);
+            _transactionsSession.DataReceived += OnTransactionReceived;
+
+            _transactionsSession.StartSession();
+
+            bool success = _transactionReceived.WaitOne(10000);
+
+            if (success)
+                WriteNewLine("Good news!. Transactions stream is functioning.");
+            else
+                WriteNewLine("Bad news!. Transactions stream is not functioning.");
+        }
+
+        protected static void OnTransactionReceived(TransactionsStreamResponse data)
+        {
+            if (!data.IsHeartbeat())
+                WriteNewLine("V20 notification - New account transaction: " + data.transaction.type);
+
+            _transactionReceived.Release();
+        }
+
+        static void StopTransactionsStream()
+        {
+            _transactionsSession.StopSession();
+        }
+        #endregion
+
+        static void WriteNewLine(string message)
+        {
+            Console.WriteLine($"\n{message}");
+        }
+    }
+}
--- OkonkwoOandaV20/OkonkwoOandaV20App/Properties/AssemblyInfo.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20App/Properties/AssemblyInfo.cs
@@ -1,34 +1,34 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OkonkwoOandaV20Tests")]
+[assembly: AssemblyTitle("OkonkwoOandaV20App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OkonkwoOandaV20Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("OkonkwoOandaV20App")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("14a55170-5f71-49a4-bea5-4b635ec5af6f")]
+[assembly: Guid("0954afb1-e5ac-4bc4-aa66-e35bf14a082b")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.*")]
