d07ebbf91b3b56662a01e86a5fd7c3ed48b52251
Osita Okonkwo <mrchrisok@hotmail.com>
2021-05-20 07:12:17 -0500

    update Instrument

--- OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/REST/GetInstrumentOrderBook.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/REST/GetInstrumentOrderBook.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using OkonkwoOandaV20.Framework.Factories;
 using OkonkwoOandaV20.TradeLibrary.Instrument;
 using System;
 using System.Threading.Tasks;
@@ -11,52 +10,63 @@ namespace OkonkwoOandaV20.TradeLibrary.REST
    /// </summary>
    public partial class Rest20
    {
-      /// <summary>
-      /// Retrieves the order book for an instrument
-      /// </summary>
-      /// <param name="instrument">Name of the Instrument [required]</param>
-      /// <param name="parameters">the parameters for the request</param>
-      /// <returns>an OrderBook object</returns>
-      public static async Task<OrderBook> GetInstrumentOrderBookAsync(string instrument, InstrumentOrderBookParameters parameters)
-      {
-         string uri = ServerUri(EServer.Account) + "instruments/" + instrument + "/orderBook";
-         var requestParams = ConvertToDictionary(parameters);
+	  /// <summary>
+	  /// Retrieves the order book for an instrument
+	  /// </summary>
+	  /// <param name="instrument">Name of the Instrument [required]</param>
+	  /// <param name="parameters">the parameters for the request</param>
+	  /// <returns>an OrderBook object</returns>
+	  public static async Task<OrderBook> GetInstrumentOrderBookAsync(string instrument, InstrumentOrderBookParameters parameters)
+	  {
+		 var request = new InstrumentOrderBookRequest()
+		 {
+			Uri = ServerUri(EServer.Account) + "instruments/" + instrument + "/orderBook",
+			Method = "GET",
+			Parameters = parameters
+		 };
 
-         InstrumentOrderBookResponse response = null;
-         try { response = await MakeRequestAsync<InstrumentOrderBookResponse>(uri, "GET", requestParams); }
-         catch (Exception ex)
-         {
-            if (parameters.getLastTimeOnFailure)
-            {
-               parameters.time = null;
-               response = await MakeRequestAsync<InstrumentOrderBookResponse, InstrumentOrderBookErrorResponse>(uri, "GET", requestParams);
-            }
-            else
-               throw ex;
-         }
+		 InstrumentOrderBookResponse response;
+		 try
+		 {
+			response = await MakeRequestAsync<InstrumentOrderBookResponse>(request);
+		 }
+		 catch (Exception ex)
+		 {
+			if (parameters.getLastTimeOnFailure)
+			{
+			   parameters.time = null;
+			   response = await MakeRequestAsync<InstrumentOrderBookResponse, InstrumentOrderBookErrorResponse>(request);
+			}
+			else
+			   throw ex;
+		 }
 
-         return response.orderBook;
-      }
+		 return response.orderBook;
+	  }
 
-      public class InstrumentOrderBookParameters
-      {
-         public InstrumentOrderBookParameters(bool getLastTimeOnFailure = true)
-         {
-            this.getLastTimeOnFailure = getLastTimeOnFailure;
-         }
+	  public class InstrumentOrderBookParameters : Parameters
+	  {
+		 public InstrumentOrderBookParameters(bool getLastTimeOnFailure = true)
+		 {
+			this.getLastTimeOnFailure = getLastTimeOnFailure;
+		 }
 
-         /// <summary>
-         /// The time of the snapshot to fetch. If not specified, then the most recent snapshot 
-         /// is fetched.
-         /// </summary>
-         public string time { get; set; }
+		 /// <summary>
+		 /// The time of the snapshot to fetch. If not specified, then the most recent snapshot 
+		 /// is fetched.
+		 /// </summary>
+		 public string time { get; set; }
 
-         /// <summary>
-         /// 
-         /// </summary>
-         [JsonIgnore]
-         public bool getLastTimeOnFailure { get; set; }
-      }
+		 /// <summary>
+		 /// 
+		 /// </summary>
+		 [JsonIgnore]
+		 public bool getLastTimeOnFailure { get; set; }
+	  }
+   }
+
+   public class InstrumentOrderBookRequest : Request
+   {
    }
 
    /// <summary>
@@ -64,10 +74,10 @@ namespace OkonkwoOandaV20.TradeLibrary.REST
    /// </summary>
    public class InstrumentOrderBookResponse : Response
    {
-      /// <summary>
-      /// The instrument’s order book
-      /// </summary>
-      public OrderBook orderBook;
+	  /// <summary>
+	  /// The instrument’s order book
+	  /// </summary>
+	  public OrderBook orderBook;
    }
 
    /// <summary>
--- OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/REST/GetInstrumentPositionBook.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/REST/GetInstrumentPositionBook.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using OkonkwoOandaV20.Framework.Factories;
 using OkonkwoOandaV20.TradeLibrary.Instrument;
 using System;
 using System.Threading.Tasks;
@@ -11,52 +10,63 @@ namespace OkonkwoOandaV20.TradeLibrary.REST
    /// </summary>
    public partial class Rest20
    {
-      /// <summary>
-      /// Fetch a position book for an instrument
-      /// </summary>
-      /// <param name="instrument">Name of the Instrument [required]</param>
-      /// <param name="parameters">the parameters for the request</param>
-      /// <returns>a PositionBook object</returns>
-      public static async Task<PositionBook> GetInstrumentPositionBookAsync(string instrument, InstrumentPositionBookParameters parameters)
-      {
-         string uri = ServerUri(EServer.Account) + "instruments/" + instrument + "/positionBook";
-         var requestParams = ConvertToDictionary(parameters);
+	  /// <summary>
+	  /// Fetch a position book for an instrument
+	  /// </summary>
+	  /// <param name="instrument">Name of the Instrument [required]</param>
+	  /// <param name="parameters">the parameters for the request</param>
+	  /// <returns>a PositionBook object</returns>
+	  public static async Task<PositionBook> GetInstrumentPositionBookAsync(string instrument, InstrumentPositionBookParameters parameters)
+	  {
+		 var request = new InstrumentPositionBookRequest()
+		 {
+			Uri = ServerUri(EServer.Account) + "instruments/" + instrument + "/positionBook",
+			Method = "GET",
+			Parameters = parameters
+		 };
 
-         InstrumentPositionBookResponse response = null;
-         try { response = await MakeRequestAsync<InstrumentPositionBookResponse, InstrumentPositionBookErrorResponse>(uri, "GET", requestParams); }
-         catch (Exception ex)
-         {
-            if (parameters.getLastTimeOnFailure)
-            {
-               parameters.time = null;
-               response = await MakeRequestAsync<InstrumentPositionBookResponse, InstrumentPositionBookErrorResponse>(uri, "GET", requestParams);
-            }            
-            else
-               throw ex;
-         }
+		 InstrumentPositionBookResponse response;
+		 try
+		 {
+			response = await MakeRequestAsync<InstrumentPositionBookResponse, InstrumentPositionBookErrorResponse>(request);
+		 }
+		 catch (Exception ex)
+		 {
+			if (parameters.getLastTimeOnFailure)
+			{
+			   parameters.time = null;
+			   response = await MakeRequestAsync<InstrumentPositionBookResponse, InstrumentPositionBookErrorResponse>(request);
+			}
+			else
+			   throw ex;
+		 }
 
-         return response.positionBook;
-      } 
+		 return response.positionBook;
+	  }
 
-      public class InstrumentPositionBookParameters
-      {
-         public InstrumentPositionBookParameters(bool getLastTimeOnFailure = true)
-         {
-            this.getLastTimeOnFailure = getLastTimeOnFailure;
-         }
+	  public class InstrumentPositionBookParameters : Parameters
+	  {
+		 public InstrumentPositionBookParameters(bool getLastTimeOnFailure = true)
+		 {
+			this.getLastTimeOnFailure = getLastTimeOnFailure;
+		 }
 
-         /// <summary>
-         /// The time of the snapshot to fetch. If not specified, then the most recent snapshot 
-         /// is fetched.
-         /// </summary>
-         public string time { get; set; }
+		 /// <summary>
+		 /// The time of the snapshot to fetch. If not specified, then the most recent snapshot 
+		 /// is fetched.
+		 /// </summary>
+		 public string time { get; set; }
 
-         /// <summary>
-         /// 
-         /// </summary>
-         [JsonIgnore]
-         public bool getLastTimeOnFailure { get; set; }
-      }
+		 /// <summary>
+		 /// 
+		 /// </summary>
+		 [JsonIgnore]
+		 public bool getLastTimeOnFailure { get; set; }
+	  }
+   }
+
+   public class InstrumentPositionBookRequest : Request
+   {
    }
 
    /// <summary>
@@ -64,10 +74,10 @@ namespace OkonkwoOandaV20.TradeLibrary.REST
    /// </summary>
    public class InstrumentPositionBookResponse : Response
    {
-      /// <summary>
-      /// The instrument’s position book
-      /// </summary>
-      public PositionBook positionBook;
+	  /// <summary>
+	  /// The instrument’s position book
+	  /// </summary>
+	  public PositionBook positionBook;
    }
 
    /// <summary>
