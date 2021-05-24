ef12ff735b0671b3ac4703b1c6dd5edc971cf50f
Osita Okonkwo <mrchrisok@hotmail.com>
2021-05-24 02:00:23 -0500

    Factories update

--- OkonkwoOandaV20/OkonkwoOandaV20/Framework/Factories/ErrorResponseFactory.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/Framework/Factories/ErrorResponseFactory.cs
@@ -5,80 +5,84 @@ namespace OkonkwoOandaV20.Framework.Factories
 {
    public class ErrorResponseFactory
    {
-      public static IErrorResponse Create(string message)
-      {
-         var dynamicError = JsonConvert.DeserializeObject<dynamic>(message);
+	  public static IErrorResponse Create(string message)
+	  {
+		 var dynamicError = JsonConvert.DeserializeObject<dynamic>(message);
 
-         // the 'type' property is injected by the Rest20.GetWebResponse catch block
+		 // the 'type' property is injected by the Rest20.GetWebResponse catch block
 
-         switch (dynamicError.type.ToString())
-         {
-            case "AccountChangesErrorResponse":
-               return JsonConvert.DeserializeObject<AccountChangesErrorResponse>(message);
-            case "AccountConfigurationErrorResponse":
-               return JsonConvert.DeserializeObject<AccountConfigurationErrorResponse>(message);
-            case "AccountErrorResponse":
-               return JsonConvert.DeserializeObject<AccountErrorResponse>(message);
-            case "AccountsErrorResponse":
-               return JsonConvert.DeserializeObject<AccountsErrorResponse>(message);
-            case "AccountInstrumentsErrorResponse":
-               return JsonConvert.DeserializeObject<AccountInstrumentsErrorResponse>(message);
-            case "AccountSummaryErrorResponse":
-               return JsonConvert.DeserializeObject<AccountSummaryErrorResponse>(message);
-            case "InstrumentCandlesErrorResponse":
-               return JsonConvert.DeserializeObject<InstrumentCandlesErrorResponse>(message);
-            case "InstrumentOrderBookErrorResponse":
-               return JsonConvert.DeserializeObject<InstrumentOrderBookErrorResponse>(message);
-            case "InstrumentPositionBookErrorResponse":
-               return JsonConvert.DeserializeObject<InstrumentPositionBookErrorResponse>(message);
-            case "OpenPositionsErrorResponse":
-               return JsonConvert.DeserializeObject<OpenPositionsErrorResponse>(message);
-            case "OpenTradesErrorResponse":
-               return JsonConvert.DeserializeObject<OpenTradesErrorResponse>(message);
-            case "OrderCancelErrorResponse":
-               return JsonConvert.DeserializeObject<OrderCancelErrorResponse>(message);
-            case "OrderReplaceErrorResponse":
-               return JsonConvert.DeserializeObject<OrderReplaceErrorResponse>(message);
-            case "OrderClientExtensionsErrorResponse":
-               return JsonConvert.DeserializeObject<OrderClientExtensionsErrorResponse>(message);
-            case "OrderErrorResponse":
-               return JsonConvert.DeserializeObject<OrderErrorResponse>(message);
-            case "OrdersErrorResponse":
-               return JsonConvert.DeserializeObject<OrdersErrorResponse>(message);
-            case "PendingOrdersErrorResponse":
-               return JsonConvert.DeserializeObject<PendingOrdersErrorResponse>(message);
-            case "PositionErrorResponse":
-               return JsonConvert.DeserializeObject<PositionErrorResponse>(message);
-            case "PositionsErrorResponse":
-               return JsonConvert.DeserializeObject<PositionsErrorResponse>(message);
-            case "PostOrderErrorResponse":
-               return JsonConvert.DeserializeObject<PostOrderErrorResponse>(message);
-            case "PositionCloseErrorResponse":
-               return JsonConvert.DeserializeObject<PositionCloseErrorResponse>(message);
-            case "PricingErrorResponse":
-               return JsonConvert.DeserializeObject<PricingErrorResponse>(message);
-            case "TradeErrorResponse":
-               return JsonConvert.DeserializeObject<TradeErrorResponse>(message);
-            case "TradesErrorResponse":
-               return JsonConvert.DeserializeObject<TradesErrorResponse>(message);
-            case "TradeClientExtensionsErrorResponse":
-               return JsonConvert.DeserializeObject<TradeClientExtensionsErrorResponse>(message);
-            case "TradeCloseErrorResponse":
-               return JsonConvert.DeserializeObject<TradeCloseErrorResponse>(message);
-            case "TradeOrdersErrorResponse":
-               return JsonConvert.DeserializeObject<TradeOrdersErrorResponse>(message);
-            case "TransactionErrorResponse":
-               return JsonConvert.DeserializeObject<TransactionErrorResponse>(message);
-            case "TransactionsByIdRangeErrorResponse":
-               return JsonConvert.DeserializeObject<TransactionsByIdRangeErrorResponse>(message);
-            case "TransactionPagesErrorResponse":
-               return JsonConvert.DeserializeObject<TransactionPagesErrorResponse>(message);
-            case "TransactionsSinceIdRangeErrorResponse":
-               return JsonConvert.DeserializeObject<TransactionsSinceIdRangeErrorResponse>(message);
+		 switch (dynamicError.type.ToString())
+		 {
+			case nameof(AccountChangesErrorResponse):
+			   return JsonConvert.DeserializeObject<AccountChangesErrorResponse>(message);
+			case nameof(AccountConfigurationErrorResponse):
+			   return JsonConvert.DeserializeObject<AccountConfigurationErrorResponse>(message);
+			case nameof(AccountErrorResponse):
+			   return JsonConvert.DeserializeObject<AccountErrorResponse>(message);
+			case nameof(AccountsErrorResponse):
+			   return JsonConvert.DeserializeObject<AccountsErrorResponse>(message);
+			case nameof(AccountInstrumentsErrorResponse):
+			   return JsonConvert.DeserializeObject<AccountInstrumentsErrorResponse>(message);
+			case nameof(AccountSummaryErrorResponse):
+			   return JsonConvert.DeserializeObject<AccountSummaryErrorResponse>(message);
+			case nameof(InstrumentCandlesErrorResponse):
+			   return JsonConvert.DeserializeObject<InstrumentCandlesErrorResponse>(message);
+			case nameof(InstrumentOrderBookErrorResponse):
+			   return JsonConvert.DeserializeObject<InstrumentOrderBookErrorResponse>(message);
+			case nameof(InstrumentPositionBookErrorResponse):
+			   return JsonConvert.DeserializeObject<InstrumentPositionBookErrorResponse>(message);
+			case nameof(OpenPositionsErrorResponse):
+			   return JsonConvert.DeserializeObject<OpenPositionsErrorResponse>(message);
+			case nameof(OpenTradesErrorResponse):
+			   return JsonConvert.DeserializeObject<OpenTradesErrorResponse>(message);
+			case nameof(OrderCancelErrorResponse):
+			   return JsonConvert.DeserializeObject<OrderCancelErrorResponse>(message);
+			case nameof(OrderReplaceErrorResponse):
+			   return JsonConvert.DeserializeObject<OrderReplaceErrorResponse>(message);
+			case nameof(OrderClientExtensionsErrorResponse):
+			   return JsonConvert.DeserializeObject<OrderClientExtensionsErrorResponse>(message);
+			case nameof(OrderErrorResponse):
+			   return JsonConvert.DeserializeObject<OrderErrorResponse>(message);
+			case nameof(OrdersErrorResponse):
+			   return JsonConvert.DeserializeObject<OrdersErrorResponse>(message);
+			case nameof(PendingOrdersErrorResponse):
+			   return JsonConvert.DeserializeObject<PendingOrdersErrorResponse>(message);
+			case nameof(PositionErrorResponse):
+			   return JsonConvert.DeserializeObject<PositionErrorResponse>(message);
+			case nameof(PositionsErrorResponse):
+			   return JsonConvert.DeserializeObject<PositionsErrorResponse>(message);
+			case nameof(PostOrderErrorResponse):
+			   return JsonConvert.DeserializeObject<PostOrderErrorResponse>(message);
+			case nameof(PositionCloseErrorResponse):
+			   return JsonConvert.DeserializeObject<PositionCloseErrorResponse>(message);
+			case nameof(PricingCandlesErrorResponse):
+			   return JsonConvert.DeserializeObject<PricingCandlesErrorResponse>(message);
+			case nameof(PricingErrorResponse):
+			   return JsonConvert.DeserializeObject<PricingErrorResponse>(message);
+			case nameof(PricingInstrumentCandlesErrorResponse):
+			   return JsonConvert.DeserializeObject<PricingInstrumentCandlesErrorResponse>(message);
+			case nameof(TradeErrorResponse):
+			   return JsonConvert.DeserializeObject<TradeErrorResponse>(message);
+			case nameof(TradesErrorResponse):
+			   return JsonConvert.DeserializeObject<TradesErrorResponse>(message);
+			case nameof(TradeClientExtensionsErrorResponse):
+			   return JsonConvert.DeserializeObject<TradeClientExtensionsErrorResponse>(message);
+			case nameof(TradeCloseErrorResponse):
+			   return JsonConvert.DeserializeObject<TradeCloseErrorResponse>(message);
+			case nameof(TradeOrdersErrorResponse):
+			   return JsonConvert.DeserializeObject<TradeOrdersErrorResponse>(message);
+			case nameof(TransactionErrorResponse):
+			   return JsonConvert.DeserializeObject<TransactionErrorResponse>(message);
+			case nameof(TransactionsByIdRangeErrorResponse):
+			   return JsonConvert.DeserializeObject<TransactionsByIdRangeErrorResponse>(message);
+			case nameof(TransactionPagesErrorResponse):
+			   return JsonConvert.DeserializeObject<TransactionPagesErrorResponse>(message);
+			case nameof(TransactionsSinceIdErrorResponse):
+			   return JsonConvert.DeserializeObject<TransactionsSinceIdErrorResponse>(message);
 
-            default:
-               return JsonConvert.DeserializeObject<ErrorResponse>(message);
-         }
-      }
+			default:
+			   return JsonConvert.DeserializeObject<ErrorResponse>(message);
+		 }
+	  }
    }
 }
