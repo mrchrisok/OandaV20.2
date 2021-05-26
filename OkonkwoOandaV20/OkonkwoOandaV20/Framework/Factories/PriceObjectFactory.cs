37dc33e63b4ce0470eb1eee6cfc23659f18c3d1b
Osita Okonkwo <mrchrisok@hotmail.com>
2021-05-26 13:53:00 -0500

    Revert "Factories updates"
    This reverts commit 4e1926f8aad5b46102b494680406bbacfe634647.

--- OkonkwoOandaV20/OkonkwoOandaV20/Framework/Factories/PriceObjectFactory.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/Framework/Factories/PriceObjectFactory.cs
@@ -4,22 +4,22 @@ namespace OkonkwoOandaV20.Framework.Factories
 {
    public class PriceObjectFactory
    {
-	  public static IHasPrices Create(string type)
-	  {
-		 IHasPrices priceObject;
+      public static IHasPrices Create(string type)
+      {
+         IHasPrices priceObject;
 
-		 switch (type)
-		 {
-			case "OkonkwoOandaV20.TradeLibrary.Transaction.TakeProfitDetails":
-			   priceObject = new TakeProfitDetails(); break;
-			case "OkonkwoOandaV20.TradeLibrary.Transaction.StopLossDetails":
-			   priceObject = new StopLossDetails(); break;
-			case "OkonkwoOandaV20.TradeLibrary.Transaction.TrailingStopLossDetails":
-			   priceObject = new TrailingStopLossDetails(); break;
-			default: priceObject = null; break;
-		 }
+         switch (type)
+         {
+            case "OkonkwoOandaV20.TradeLibrary.Transaction.TakeProfitDetails":
+               priceObject = new TakeProfitDetails(); break;
+            case "OkonkwoOandaV20.TradeLibrary.Transaction.StopLossDetails":
+               priceObject = new StopLossDetails(); break;
+            case "OkonkwoOandaV20.TradeLibrary.Transaction.TrailingStopLossDetails":
+               priceObject = new TrailingStopLossDetails(); break;
+            default: priceObject = null; break;
+         }
 
-		 return priceObject;
-	  }
+         return priceObject;
+      }
    }
 }
