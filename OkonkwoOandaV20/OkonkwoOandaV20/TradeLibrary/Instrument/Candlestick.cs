f0b746a36bb1236e6403fcf1915a59b18656f77f
Osita Okonkwo <mrchrisok@hotmail.com>
2021-05-29 07:58:14 -0500

    Instrument update to DateTime

--- OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/Candlestick.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/Candlestick.cs
@@ -1,28 +1,30 @@
-﻿namespace OkonkwoOandaV20.TradeLibrary.Instrument
+﻿using System;
+
+namespace OkonkwoOandaV20.TradeLibrary.Instrument
 {
    public class Candlestick
    {
-      public string time { get; set; }
-      public CandleStickData bid { get; set; }
-      public CandleStickData ask { get; set; }
-      public CandleStickData mid { get; set; }
-      public int volume { get; set; }
-      public bool complete { get; set; }
+	  public DateTime time { get; set; }
+	  public CandleStickData bid { get; set; }
+	  public CandleStickData ask { get; set; }
+	  public CandleStickData mid { get; set; }
+	  public int volume { get; set; }
+	  public bool complete { get; set; }
    }
 
    public class CandlestickPlus : Candlestick
    {
-      public CandlestickPlus() { }
-      public CandlestickPlus(Candlestick candlestick)
-      {
-         time = candlestick.time;
-         bid = candlestick.bid;
-         ask = candlestick.ask;
-         mid = candlestick.mid;
-         volume = candlestick.volume;
-         complete = candlestick.complete;
-      }
-      public string instrument { get; set; }
-      public string granularity { get; set; }
+	  public CandlestickPlus() { }
+	  public CandlestickPlus(Candlestick candlestick)
+	  {
+		 time = candlestick.time;
+		 bid = candlestick.bid;
+		 ask = candlestick.ask;
+		 mid = candlestick.mid;
+		 volume = candlestick.volume;
+		 complete = candlestick.complete;
+	  }
+	  public string instrument { get; set; }
+	  public string granularity { get; set; }
    }
 }
--- OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/PositionBook.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Instrument/PositionBook.cs
@@ -1,36 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace OkonkwoOandaV20.TradeLibrary.Instrument
 {
    public class PositionBook
    {
-      /// <summary>
-      /// The position book’s instrument
-      /// </summary>
-      public string instrument { get; set; }
+	  /// <summary>
+	  /// The position book’s instrument
+	  /// </summary>
+	  public string instrument { get; set; }
 
-      /// <summary>
-      /// The time when the position book snapshot was created.
-      /// </summary>
-      public string time { get; set; }
+	  /// <summary>
+	  /// The time when the position book snapshot was created.
+	  /// </summary>
+	  public DateTime time { get; set; }
 
-      /// <summary>
-      /// The price (midpoint) for the position book’s instrument at the time of the 
-      /// order book snapshot
-      /// </summary>
-      public decimal price { get; set; }
+	  /// <summary>
+	  /// The price (midpoint) for the position book’s instrument at the time of the 
+	  /// order book snapshot
+	  /// </summary>
+	  public decimal price { get; set; }
 
-      /// <summary>
-      /// The price width for each bucket. Each bucket covers the price range from 
-      /// the bucket’s price to the bucket’s price + bucketWidth.
-      /// </summary>
-      public decimal bucketWidth { get; set; }
+	  /// <summary>
+	  /// The price width for each bucket. Each bucket covers the price range from 
+	  /// the bucket’s price to the bucket’s price + bucketWidth.
+	  /// </summary>
+	  public decimal bucketWidth { get; set; }
 
-      /// <summary>
-      /// The partitioned position book, divided into buckets using a default bucket
-      /// width. These buckets are only provided for price ranges which actually
-      /// contain order or position data.
-      /// </summary>
-      public List<OrderBookBucket> buckets { get; set; }
+	  /// <summary>
+	  /// The partitioned position book, divided into buckets using a default bucket
+	  /// width. These buckets are only provided for price ranges which actually
+	  /// contain order or position data.
+	  /// </summary>
+	  public List<OrderBookBucket> buckets { get; set; }
    }
 }
